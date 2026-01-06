bb80a256ada5cd9f0ef50fc0371d65fec72f18b5
badal <badalboss2@gmail.com>
2026-01-06 16:35:41 +0530

    Fix server corruption and robustify passport config

--- server/config/passport.js
+++ server/config/passport.js
@@ -32,95 +32,103 @@ const configurePassport = () => {
     }));
 
     // GitHub
-    passport.use(new GitHubStrategy({
-        clientID: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: process.env.GITHUB_CALLBACK_URL || `${BACKEND_URL}/auth/github/callback`
-    }, async (accessToken, refreshToken, profile, done) => {
-        try {
-            let user = await User.findOne({ githubId: profile.id });
-            if (!user) {
-                user = await User.create({
-                    name: profile.displayName || profile.username,
-                    email: profile.emails ? profile.emails[0].value : `${profile.username}@github.com`,
-                    githubId: profile.id,
-                    avatar: profile.photos[0].value
-                });
+    if (process.env.GITHUB_CLIENT_ID) {
+        passport.use(new GitHubStrategy({
+            clientID: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            callbackURL: process.env.GITHUB_CALLBACK_URL || `${BACKEND_URL}/auth/github/callback`
+        }, async (accessToken, refreshToken, profile, done) => {
+            try {
+                let user = await User.findOne({ githubId: profile.id });
+                if (!user) {
+                    user = await User.create({
+                        name: profile.displayName || profile.username,
+                        email: profile.emails ? profile.emails[0].value : `${profile.username}@github.com`,
+                        githubId: profile.id,
+                        avatar: profile.photos[0].value
+                    });
+                }
+                return done(null, user);
+            } catch (err) {
+                return done(err, null);
             }
-            return done(null, user);
-        } catch (err) {
-            return done(err, null);
-        }
-    }));
+        }));
+    }
 
     // Facebook
-    passport.use(new FacebookStrategy({
-        clientID: process.env.FACEBOOK_CLIENT_ID,
-        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-        callbackURL: process.env.FACEBOOK_CALLBACK_URL || `${BACKEND_URL}/auth/facebook/callback`,
-        profileFields: ['id', 'displayName', 'photos', 'email']
-    }, async (accessToken, refreshToken, profile, done) => {
-        try {
-            let user = await User.findOne({ facebookId: profile.id });
-            if (!user) {
-                user = await User.create({
-                    name: profile.displayName,
-                    email: profile.emails ? profile.emails[0].value : `${profile.id}@facebook.com`,
-                    facebookId: profile.id,
-                    avatar: profile.photos ? profile.photos[0].value : null
-                });
+    if (process.env.FACEBOOK_CLIENT_ID) {
+        passport.use(new FacebookStrategy({
+            clientID: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+            callbackURL: process.env.FACEBOOK_CALLBACK_URL || `${BACKEND_URL}/auth/facebook/callback`,
+            profileFields: ['id', 'displayName', 'photos', 'email']
+        }, async (accessToken, refreshToken, profile, done) => {
+            try {
+                let user = await User.findOne({ facebookId: profile.id });
+                if (!user) {
+                    user = await User.create({
+                        name: profile.displayName,
+                        email: profile.emails ? profile.emails[0].value : `${profile.id}@facebook.com`,
+                        facebookId: profile.id,
+                        avatar: profile.photos ? profile.photos[0].value : null
+                    });
+                }
+                return done(null, user);
+            } catch (err) {
+                return done(err, null);
             }
-            return done(null, user);
-        } catch (err) {
-            return done(err, null);
-        }
-    }));
+        }));
+    }
 
     // LinkedIn
-    passport.use(new LinkedInStrategy({
-        clientID: process.env.LINKEDIN_CLIENT_ID,
-        clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-        callbackURL: process.env.LINKEDIN_CALLBACK_URL || `${BACKEND_URL}/auth/linkedin/callback`,
-        scope: ['r_liteprofile', 'r_emailaddress'],
-    }, async (accessToken, refreshToken, profile, done) => {
-        try {
-            let user = await User.findOne({ linkedinId: profile.id });
-            if (!user) {
-                user = await User.create({
-                    name: profile.displayName,
-                    email: profile.emails && profile.emails.length > 0 ? profile.emails[0].value : "",
-                    linkedinId: profile.id,
-                    avatar: profile.photos && profile.photos.length > 0 ? profile.photos[0].value : null
-                });
+    if (process.env.LINKEDIN_CLIENT_ID) {
+        passport.use(new LinkedInStrategy({
+            clientID: process.env.LINKEDIN_CLIENT_ID,
+            clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+            callbackURL: process.env.LINKEDIN_CALLBACK_URL || `${BACKEND_URL}/auth/linkedin/callback`,
+            scope: ['r_liteprofile', 'r_emailaddress'],
+        }, async (accessToken, refreshToken, profile, done) => {
+            try {
+                let user = await User.findOne({ linkedinId: profile.id });
+                if (!user) {
+                    user = await User.create({
+                        name: profile.displayName,
+                        email: profile.emails && profile.emails.length > 0 ? profile.emails[0].value : "",
+                        linkedinId: profile.id,
+                        avatar: profile.photos && profile.photos.length > 0 ? profile.photos[0].value : null
+                    });
+                }
+                return done(null, user);
+            } catch (err) {
+                return done(err, null);
             }
-            return done(null, user);
-        } catch (err) {
-            return done(err, null);
-        }
-    }));
+        }));
+    }
 
     // Twitter
-    passport.use(new TwitterStrategy({
-        clientID: process.env.TWITTER_CLIENT_ID,
-        clientSecret: process.env.TWITTER_CLIENT_SECRET || 'none', // Handle empty secret
-        callbackURL: process.env.TWITTER_CALLBACK_URL || `${BACKEND_URL}/auth/twitter/callback`,
-        clientType: 'confidential'
-    }, async (accessToken, refreshToken, profile, done) => {
-        try {
-            let user = await User.findOne({ twitterId: profile.id });
-            if (!user) {
-                user = await User.create({
-                    name: profile.displayName,
-                    email: profile.emails ? profile.emails[0].value : `${profile.username}@twitter.com`,
-                    twitterId: profile.id,
-                    avatar: profile.photos ? profile.photos[0].value : null
-                });
+    if (process.env.TWITTER_CLIENT_ID) {
+        passport.use(new TwitterStrategy({
+            clientID: process.env.TWITTER_CLIENT_ID,
+            clientSecret: process.env.TWITTER_CLIENT_SECRET || 'none', // Handle empty secret
+            callbackURL: process.env.TWITTER_CALLBACK_URL || `${BACKEND_URL}/auth/twitter/callback`,
+            clientType: 'confidential'
+        }, async (accessToken, refreshToken, profile, done) => {
+            try {
+                let user = await User.findOne({ twitterId: profile.id });
+                if (!user) {
+                    user = await User.create({
+                        name: profile.displayName,
+                        email: profile.emails ? profile.emails[0].value : `${profile.username}@twitter.com`,
+                        twitterId: profile.id,
+                        avatar: profile.photos ? profile.photos[0].value : null
+                    });
+                }
+                return done(null, user);
+            } catch (err) {
+                return done(err, null);
             }
-            return done(null, user);
-        } catch (err) {
-            return done(err, null);
-        }
-    }));
+        }));
+    }
 
     passport.serializeUser((user, done) => {
         done(null, user.id);
